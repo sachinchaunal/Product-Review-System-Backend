a0fb95836f8b186a8c087fc6abd0b32251bf2104
Sachin Chaunal <155889006+sachinchaunal@users.noreply.github.com>
2025-03-30 10:45:28 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 // Configure CORS to allow requests from frontend
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.FRONTEND_URL || 'https://reviewsphere.vercel.app',
   credentials: true
 }));
 
